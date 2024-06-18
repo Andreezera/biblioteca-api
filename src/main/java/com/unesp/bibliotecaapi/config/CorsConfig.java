ddef3e3ec234b9ced0314d870fcaaee79be9de5e
Andreezera <andredionisio1@hotmail.com>
2024-06-17 21:13:04 -0300

    .

--- src/main/java/com/unesp/bibliotecaapi/config/CorsConfig.java
+++ src/main/java/com/unesp/bibliotecaapi/config/CorsConfig.java
@@ -15,5 +15,15 @@ public class CorsConfig implements WebMvcConfigurer {
             .allowedHeaders("*") // Permitir todos os cabeçalhos
             .allowCredentials(false) // Permitir envio de credenciais (por exemplo, cookies)
             .maxAge(3600); // Tempo máximo de cache para opções pré-voo (em segundos)
+
+        registry.addMapping("/clientes/alunos/**") // Endpoints para alunos
+                .allowedOrigins("*") // Permitir solicitações de todos os domínios
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Permitir todos os cabeçalhos
+
+        registry.addMapping("/clientes/professores/**") // Endpoints para professores
+                .allowedOrigins("*") // Permitir solicitações de todos os domínios
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Permitir todos os cabeçalhos
     }
 }
