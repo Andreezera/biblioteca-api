618d4d37a4d23ea0b91abc886cdf1d612b48a4ee
Andreezera <andredionisio1@hotmail.com>
2024-06-17 23:30:23 -0300

    .

--- src/main/java/com/unesp/bibliotecaapi/model/Emprestimo.java
+++ src/main/java/com/unesp/bibliotecaapi/model/Emprestimo.java
@@ -1,5 +1,6 @@
 package com.unesp.bibliotecaapi.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,8 +14,11 @@ public class Emprestimo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "America/Sao_Paulo")
     private Date dataEmprestimo;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "America/Sao_Paulo")
     private Date dataPrevistaDevolucao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "America/Sao_Paulo")
     private Date dataDevolucao = null;
     private Boolean foiDevolvido;
 
